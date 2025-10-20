70e632eaa84f9eb9b2813c640177d92678e898cc
joseguilherme-zg <zansavio.goncalves@escola.pr.gov.br>
2025-10-20 09:36:23 -0300

    update

---
+++ exe1.js
@@ -0,0 +1,7 @@
+console.log('Operações aritméticas');
+
+console.log('soma:', 3+2);
+console.log('subtração:',3-2);
+console.log('divisão:',3/2);
+console.log('multiplicação:',3*2);
+console.log('resto da divisão:',3%2);
